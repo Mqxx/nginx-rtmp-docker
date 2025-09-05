83fe73a0c217e90f9a0b4de6bdf696ea671776b5
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-09-05 18:24:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,11 @@
 set -e
 
 mkdir -p /etc/nginx/defaults
-wget https://raw.githubusercontent.com/nginx/nginx/master/conf/mime.types -O /etc/nginx/defaults/mime.types
 touch /etc/nginx/defaults/nginx.conf
 
-if [ -z "$(ls -A /etc/nginx 2>/dev/null)" ]; then
-  echo "Empty /etc/nginx directory, copying defaults..."
-  cp -n /etc/nginx/defaults/* /etc/nginx/
+if [ ! -f /etc/nginx/nginx.conf ]; then
+  echo "No nginx.conf found, copying defaults..."
+  cp -a /etc/nginx/defaults/. /etc/nginx/
 fi
 
 exec "$@"
